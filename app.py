fcd3cc7c0b9da45e91b8bd3620892fb4969ce138
Tanisha Banik <26tanishabanik@gmail.com>
2022-02-27 12:31:12 +0530

    removed date from select aoi parameters page

--- app.py
+++ app.py
@@ -152,51 +152,8 @@ elif option == 'Select AOI Data Parameters':
 
     lat = st.number_input('Latitude', min_value =23.0063 ,format="%.4f")
     
-    col3,_ = st.columns((1,2)) # To make it narrower
-    
-    format = 'MMM DD, YYYY'  # format output
-        
-    start1 = datetime.date(year=2020,month=1,day=1)-relativedelta(years=1) #  I need some range in the past
-
-    start2 = datetime.date(year=2021,month=11,day=1)
-    st.text("")
-    st.text("")
-    
-    
-    st.text("")
-    st.text("")
-
-    
-    st.text("")
-    st.text("")
-    
-    max_days = start2-start1
-        
-    slider1 = col3.slider('Select Start Date', min_value=start1, value=start2 ,max_value=start2, format=format)
-        
-    st.table(pd.DataFrame([[start1, slider1,start2]],
-                      columns=['start1',
-                               'start_selected',
-                               'start2'],
-                      index=['date']))
-
-    end1 = datetime.date(year=2020,month=1,day=15)-relativedelta(years=1) #  I need some range in the past
-    
-    end2 = datetime.date(year=2021,month=12,day=31)
-    
-    max_days = end2-end1
-        
-    slider2 = col3.slider('Select End Date', min_value=end1, value=end2, max_value=end2, format=format)
-        
-    st.table(pd.DataFrame([[end1, slider2, end2]],
-                      columns=['end1',
-                               'end_selected',
-                               'end2'],
-                      index=['date']))
-    
     
 
-    
     if st.button('Submit'):
         
         st.write("Submitted")
